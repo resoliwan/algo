87856d8452ae0ab8c817eafea29e58a63edfb589
resoliwan <resoliwan@naver.com>
2017-02-17 13:24:28 +0900

    start of python algorithms study

---
+++ temp/1_7_review_of_basic_pyhon.py
@@ -0,0 +1,47 @@
+print("Algorithms and data structures")
+print(7/3)
+print(7//3)
+print(7%3)
+True
+False
+True or False
+not (True)
+True and False
+print((5 >= 1) and (5 <= 10))
+theSum = 0
+theSum = theSum + 1
+theSum
+theSum = True
+myList = [1]
+myList
+myList * 2
+print(0 in myList)
+print(1 in myList)
+len(myList)
+[0,1,2,3][0:1]
+myList = [0] * 6
+A = [myList] * 3
+A
+myList[0] = 'A'
+A
+
+myList = [0,1,2,3,1]
+myList.index(1)
+myList.count(1)
+myList.remove(1)
+myList
+myList.remove(1)
+myList
+myList = [0,1,2,3,1]
+myList.reverse()
+myList.sort()
+myList
+myName = "David"
+myName.center(10)
+myName.find('v')
+myName.split('v')
+myTupple = (2, True, 4.96)
+# myTupple[0] = 3
+mySet = {3,True,"A", "A"}
+aName = input("Please enter your name")
+print("hi", aName)
