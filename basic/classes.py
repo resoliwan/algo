95362f11cf21dc866bc7c243ccdf740eae531758
resoliwan <resoliwan@naver.com>
2017-02-26 22:02:29 +0900

    done

--- basic/classes.py
+++ basic/classes.py
@@ -15,22 +15,61 @@ class Fraction:
         """print str"""
         return str(self.num)+"/"+str(self.den)
 
-    def gcd(m, n):
-        while m%n != 0:
+    def gcd(self, m, n):
+        while m % n != 0:
             oldm = m
             oldn = n
 
             m = oldn
-            n = oldm%oldn
+            n = oldm % oldn
         return n
 
-    def __add__(self, otherFraction):
+    def __add__(self, otherfraction):
 
-        new_num = self.num * otherFraction.den + otherFraction.num * self.den
-        new_den = self.den * otherFraction.den
-        common = gcd(new_num, new_den)
+        new_num = self.num * otherfraction.den + otherfraction.num * self.den
+        new_den = self.den * otherfraction.den
+        common = self.gcd(new_num, new_den)
 
         return Fraction(new_num//common, new_den//common)
+
+    def __eq__(self, otherfraction):
+        firstnum = self.num * otherfraction.den
+        secondnum = otherfraction.num * self.den
+
+        return firstnum == secondnum
+
+    def __mul__(self, other):
+        num = self.num * other.num
+        den = self.den * other.den
+        common = self.gcd(num, den)
+        return Fraction(num//common, den//common)
+
+    def __truediv__(self, other):
+        num = self.num * other.den
+        den = self.den * other.num
+        common = self.gcd(num, den)
+
+        return Fraction(num//common, den//common)
+
+    def __sub__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        new_num = firstnum - secondnum
+        new_den = self.den * other.den
+        common = self.gcd(new_num, new_den)
+
+        return Fraction(new_num//common, new_den//common)
+
+    def __lt__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum < secondnum
+
+    def __gt__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+        return firstnum > secondnum
 
 
 myfraction = Fraction(3, 5)
@@ -42,6 +81,14 @@ str(myfraction)
 
 f1 = Fraction(1, 4)
 f2 = Fraction(1, 2)
-f3=f1+f2
+f3 = f1 + f2
 print(f3)
+
+f4 = Fraction(1, 4)
+f5 = Fraction(1, 4)
+print(f4 == f5)
+
+f1 < f2
+f1 > f2
+
 
