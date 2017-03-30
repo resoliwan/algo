b1a1e1ad93e23c6e8b116c0c08094459881ff52b
resoliwan <resoliwan@naver.com>
2017-03-30 10:48:15 +0900

    add queue

--- basic/infix_postfix.py
+++ basic/infix_postfix.py
@@ -26,10 +26,37 @@ def infixToPostFix2(infixexpr):
     return "".join(output)
 
 
-print(infixToPostFix2("A+B*C"))
 print(infixToPostFix2("A+B*C+D"))
 print(infixToPostFix2("(A+B)*C+D"))
 print(infixToPostFix2("( A + B ) * ( C + D )"))
 print(infixToPostFix2("( A + B ) * C"))
 print(infixToPostFix2("A + B * C"))
 
+
+def doMath(operator, operand1, operand2):
+    if operator == "+":
+        return operand1 + operand2
+    elif operator == "-":
+        return operand1 - operand2
+    elif operator == "*":
+        return operand1 * operand2
+    elif operator == "/":
+        return operand1 / operand2
+
+
+def postfixEval(postfixExpr):
+    opStack = Stack()
+    tokens = list(postfixExpr)
+    for token in tokens:
+        if token in "0123456789":
+            opStack.push(int(token))
+        elif token in "+-*/":
+            operand2 = opStack.pop()
+            operand1 = opStack.pop()
+            result = doMath(token, operand1, operand2)
+            opStack.push(result)
+    return opStack.pop()
+
+
+print(postfixEvaluation("12+"))
+print(postfixEvaluation("78+32+/"))
---
+++ basic/queue.py
@@ -0,0 +1,35 @@
+# Queue
+# Queue Create a new queue that is empty 
+#    it needs no parameter and returns an empty queue.
+# enqueue(item) Add a new item at the rear of the queue. It need the item and
+#  returns nothing
+# dequeue() Remove the front item from the queue. It need no parameters and
+#  return the item.
+# isEmpty() Tests to see whether queue is empty. It need no parmeters  
+#  and return boolean values
+# size() Return the number of items in the queue. It need no parameters 
+#  and return Integer.
+
+
+class Queue():
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def ieEmpty(self):
+        return self.items == [] 
+
+    def size(self):
+        return len(self.items)
+        
+
+q = Queue()
+q.enqueue('hello')
+q.enqueue('dog')
+q.enqueue(3)
+q.dequeue()
