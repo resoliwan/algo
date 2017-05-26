83d812ae4ea6380d5b9c0aab589e2aa397aeb7ef
resoliwan <resoliwan@naver.com>
2017-05-26 09:46:38 +0900

    modified:   recursion/exploring_maze.py

--- recursion/exploring_maze.py
+++ recursion/exploring_maze.py
@@ -1,27 +1,53 @@
 import turtle
+import importlib
 
-
+OBSTATCLE = '+'
 class Maze:
     def __init__(self, mazeFileName):
+        importlib.reload(turtle)
         self.rowSize = 0
         self.colSize = 0
         self.map = []
-        mazefile = open(mazeFileName)
-        self.t = turtle(shape='turtle')
-        setup(width=600, height=600)
-
+        mazeFile = open(mazeFileName, 'r')
         for rowIdx, row in enumerate(mazeFile):
-            self.rowSize = rowIdx
+            self.rowSize = rowIdx + 1
+            row = row[:-1]
             for colIdx, col in enumerate(row):
                 self.colSize = len(row)
                 if col == 'S':
                     self.startRow = rowIdx
                     self.startCol = colIdx
             self.map.append(list(row))
+        self.t = turtle.Turtle()
+        self.t.shape('turtle')
+        self.wn = turtle.Screen()
+        self.xTranslate = 0
+        self.yTranslate = self.rowSize 
+        self.wn.setworldcoordinates(0, 0, +self.colSize, +self.rowSize)
+
+    def drawMaze(self):
+        # self.wn.tracer(0)
+        for y in range(self.rowSize):
+            for x in range(self.colSize):
+                print('x: {}, y: {}, val: {}'.format(x, y, self.map[y]))
+                if self.map[y][x] == OBSTATCLE:
+                    self.drawCenteredBox(x, y, 'orange')
+
+    def drawCenteredBox(self, x, y, color):
+        self.t.up()
+        self.t.goto(x, y)
+        self.t.color('orange')
+        self.t.setheading(90)
+        self.t.down()
+        self.t.begin_fill()
+
+        for i in range(4):
+            self.t.forward(1)
+            self.t.right(90)
+
+        self.t.end_fill()
 
 
 
-
-
-
-Maze('./recursion/maze.map')
+m = Maze('./recursion/maze.map')
+m.drawMaze()
