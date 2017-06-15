1fc42b8d486d80959deb4951bb75b9a8eb5f3679
resoliwan <resoliwan@naver.com>
2017-06-15 11:45:53 +0900

    add hashTable

--- search_sort/hashing.py
+++ search_sort/hashing.py
@@ -1,7 +1,53 @@
-def hash(aString, tableSize):
-    return sum([ord(x) for x in list(aString)]) % tableSize
+# def hash(aString, tableSize):
+#     return sum([ord(x) for x in list(aString)]) % tableSize
 
-print(hash('hello', 3))
+# print(hash('hello', 3))
 # print(hash('hello1', 3))
 # print(hash('hello2', 3))
 
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None] * size
+        self.data = [None] * size
+
+    def put(self, key, value):
+        hashValue =  self.hash(key)
+        if self.slots[hashValue] == None:
+            self.slots[hashValue] = key
+            self.data[hashValue] = value
+        else:
+            if self.slots[hashValue] == key:
+                self.data[hashValue] = value #replace
+            else:
+                nextSlot = self.reHash(hashValue)
+                while self.slots[nextSlot] != None and key != self.slots[nextSlot]:
+                    nextSlot = self.reHash(nextSlot)
+
+                if self.slots[nextSlot] == None:
+                    self.slots[nextSlot] = key
+                    self.data[nextSlot] = value
+                else:
+                    self.data[nextSlot] = value #replace
+
+
+    def hash(self, key):
+        return sum([ord(ch) for ch in list(key)]) % self.size
+
+    def reHash(self, oldHash):
+        return (oldHash + 1) % self.size
+
+    def printInfo(self):
+        print(self.slots)
+        print(self.data)
+    
+m = HashTable(3)
+print(m.put('2', 2))
+print(m.printInfo())
+print(m.put('4', 4))
+print(m.printInfo())
+print(m.put('8', 8))
+print(m.printInfo())
+
+# print(m.put('10', 10))
+# print(m.printInfo())
