bafcff2888dbcebfb0bc7a7eef8d14ebd54935f7
resoliwan <resoliwan@naver.com>
2017-07-19 11:42:39 +0900

    tree class

---
+++ tree/node_and_reference.py
@@ -0,0 +1,46 @@
+class BinaryTree:
+    def __init__(self, rootObj):
+        self.key = rootObj;
+        self.leftChild = None;
+        self.rightChild = None;
+
+    def insertLeft(self, newNode):
+        if self.leftChild is None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.leftChild = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild is None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.rightChild = self.rightChild
+            self.rightChild = t
+
+    def getLeftChild(self):
+        return self.leftChild
+    
+    def getRightChild(self):
+        return self.rightChild
+
+    def getRootVal(self):
+        return self.key
+
+    def setRootVal(self, rootObj):
+        self.key = rootObj
+
+r = BinaryTree('a')
+print(r.getRootVal())
+print(r.getLeftChild())
+r.insertLeft('b')
+print(r.getLeftChild())
+print(r.getLeftChild().getRootVal())
+r.insertRight('c')
+print(r.getRightChild())
+print(r.getRightChild().getRootVal())
+r.getRightChild().setRootVal('hello')
+print(r.getRightChild().getRootVal())
+        
